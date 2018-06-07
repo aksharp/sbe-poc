9da7dcf1171553621afec1841770d4da99683143
Alexander Khotyanov <alex@khotyanov.com>
2018-06-07 12:14:38 -0400

    cleaner shell script to codegen and schema gen

--- src/main/resources/schema_to_java.sh
+++ src/main/resources/schema_to_java.sh
@@ -1,26 +1,11 @@
 #!/usr/bin/env bash
 
-#SBE_JAR=/Users/alex/.ivy2/cache/uk.co.real-logic/sbe-all/jars/sbe-all-1.8.1.jar
-
-#java -jar -Dsbe.target.language=java -Dsbe.output.dir=../java -Dsbe.generate.ir=true /Users/alex/.ivy2/cache/uk.co.real-logic/sbe-all/jars/sbe-all-1.8.1.jar schema.xml
-
-#java -jar -Dsbe.target.language=java -Dsbe.output.dir=../java /Users/alex/.ivy2/cache/uk.co.real-logic/sbe-all/jars/sbe-all-1.8.1.jar schema.sbeir
-
-
-
-
-#java -jar -Dsbe.ir.filename=schema.sbeir -Dsbe.output.dir=../ir /Users/alex/.ivy2/cache/uk.co.real-logic/sbe-all/jars/sbe-all-1.8.1.jar schema.xml
-
-#     -Dsbe.generate.stubs=false \
-#     -Dsbe.ir.filename=com.aksharp.baseline.sbeir \
-
+# path to sbe-all-1.8.1.jar likely to be different for you
 java -Dsbe.output.dir=../java  \
      -Dsbe.generate.ir=true \
      -Dsbe.target.namespace=io.github.aksharp.sbe.poc \
      -jar /Users/alex/.ivy2/cache/uk.co.real-logic/sbe-all/jars/sbe-all-1.8.1.jar \
      schema.xml
 
-
-
-
-#java -Dsbe.output.dir=../OUT -Dsbe.generate.ir=true -Dsbe.generate.stubs=false -jar /Users/alex/.ivy2/cache/uk.co.real-logic/sbe-all/jars/sbe-all-1.8.1.jar schema.xml
+# Unfortunately no command to set schema location, hence moving it back to resources here
+mv ../java/schema.sbeir .
